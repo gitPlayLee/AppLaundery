d1b40466d3ff6e45a9b694e135eee5c306a135e2
gitPlayLee <lion7542@naver.com>
2020-11-09 20:11:18 +0900

    레이아웃 명 수정

--- LaundryBible/app/src/main/java/com/example/laundrybible/TipFrag_2.java
+++ LaundryBible/app/src/main/java/com/example/laundrybible/TipFrag_2.java
@@ -12,7 +12,7 @@ public class TipFrag_2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.tip_frag_1_2, container, false);
+        View view = inflater.inflate(R.layout.tip_frag_2_1, container, false);
         return view;
     }
 }
